159e8d9dd883e6c7739091b2519756af633742d6
Léonard Blier <leonardb@fb.com>
2020-01-23 18:13:55 +0100

    datafolder

--- code/data/generation_script.py
+++ code/data/generation_script.py
@@ -13,19 +13,21 @@ parser.add_argument('--threads', type=int, help="Number of threads")
 parser.add_argument('--rootdir', type=str, help="Directory to store rollout "
                     "directories of each thread")
 parser.add_argument('--policy', type=str, choices=['brown', 'white'],
-                    help="Directory to store rollout directories of each thread",
+                    help="",
                     default='brown')
+parser.add_argument('--environment', type=str, choices=['carracing', 'lunarlander'], default='carracing')
 parser.add_argument('--logdir', type=str)
 args = parser.parse_args()
 
 rpt = args.rollouts // args.threads + 1
 
+print("test")
 def _threaded_generation(i):
     tdir = join(args.rootdir, 'thread_{}'.format(i))
     makedirs(tdir, exist_ok=True)
     cmd = ['xvfb-run', '-s', '"-screen 0 1400x900x24"']
     cmd += ['--server-num={}'.format(i + 1)]
-    cmd += ["python -m", "data.carracing", "--dir",
+    cmd += ["python -m", f"data.{args.environment}", "--dir",
             tdir, "--rollouts", str(rpt), "--policy", args.policy]
     cmd = " ".join(cmd)
     print(cmd)
